0c2ac9b1eacb6c1778554f0913a5781e216665ba
limaxx <pkedditsoficial@gmail.com>
2026-03-12 00:10:09 -0300

    Updated projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,7 +12,7 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 1,
-    title: 'oi',
+    title: 'Sistema de Agendamento - Barbearia',
     description: 'Website desenvolvido para uma barbearia com foco em presença online e agendamento de clientes, O projeto inclui interface moderna e integração com backend utilizando Supabase para gerenciamento de dados.',
     technologies: ['React', 'TypeScript', 'Tailwind CSS', 'Vite'],
     category: 'web',
